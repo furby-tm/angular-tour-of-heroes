1554b01959ee37dc00db3adb964991a709202de8
Tyler Furby <tyler@tylerfurby.com>
2017-07-01 16:58:51 -0600

    Implemented Asynchronous loading for Heroes

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
+})
+
+export class HeroesComponent implements OnInit {
+
+  title = "Tour of Heroes";
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
