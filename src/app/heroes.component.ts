d952e43ce182bb9f28a78ce39f46e0956b3b1c35
Tyler Furby <tyler@tylerfurby.com>
2017-07-01 19:17:54 -0600

    Added routing / split components

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -5,14 +5,11 @@ import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-heroes',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
-
-  title = "Tour of Heroes";
 
   heroes: Hero[];
   selectedHero: Hero;
